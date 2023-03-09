99ac72d12efafc23c4ebe74d05f736c6cf0bc96a
vivaan187 <127401168+vivaan187@users.noreply.github.com>
2023-03-09 20:19:34 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var box
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(100,250,40,50)
+}
+
+function draw() 
+{
+  background("red");
+  drawSprites();
+  if (keyIsDown(RIGHT_ARROW)) {
+  box.position.x=box.position.x+5;
+  }
+
+  if (keyIsDown(LEFT_ARROW)) {
+  box.position.x=box.position.x-5;
+  }
+
+  if (keyIsDown(UP_ARROW)) {
+   box.position.y=box.position.y-5;
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+  box.position.y=box.position.y+5;
+  }
+
+}
+
+
+
+
